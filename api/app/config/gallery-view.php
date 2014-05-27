bcb0ab07fd7783928f1f60a511ee3943aa4f94b4
Michael Stork <michael@mstork.com>
2014-05-27 16:25:26 -0400

    formatting

--- api/app/config/gallery-view.php
+++ api/app/config/gallery-view.php
@@ -1,16 +1,23 @@
 <?php
 
     return array(
+
         // full resolution photos
         'images_path' => '/home/michael/gallery-view/photos/',
+
         // thumbnails directory - must be public
         'thumbnails_path' => '/home/michael/gallery-view/api/public/images/thumbnails/',
+
         // uri to thumbnails directory
         'thumbnails_uri'  => asset('images/thumbnails') . '/',
+
         // maximum height of generated thumbnails
         'thumbnail_max_height' => 400,
+
         // number of photos to return when requesting gallery previews
         'preview_length' => 3,
+
         // caching
         'enable_cache' => TRUE
+
     );
